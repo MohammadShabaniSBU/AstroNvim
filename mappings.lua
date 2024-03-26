4f40107afa0630b7583fc298bb73152a1501c8d5
Mohammad Shabani <mohamad@defraged.com>
2024-03-27 01:49:38 +0330

    feat: add flutter

--- mappings.lua
+++ mappings.lua
@@ -36,6 +36,8 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>Fs"] = { "<cmd>FlutterRun<cr>", desc = "Run flutter app" },
+    ["<leader>Fr"] = { "<cmd>FlutterRestart<cr>", desc = "Hot restart flutter app" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
